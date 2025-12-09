47c815ce9fff03d017bcba63732baf1438f33e7c
phum1599999 <6811130010@MUT.AC.TH>
2025-12-09 16:03:02 +0700

    อัพยา LAB4

---
+++ LAB04/Q5/Q5.cpp
@@ -0,0 +1,43 @@
+// Q5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int longDaoDuSi = 42;
+    int DaoDao;
+
+    cout << "เกมทายเลข 1 - 100\n";
+
+    while (true) {
+        cout << "ลองเดาดู: ";
+        cin >> DaoDao;
+
+        if (DaoDao == longDaoDuSi) {
+            cout << "ถูกต้อง เลขคือ " << longDaoDuSi << endl;
+            break;
+        }
+        else if (DaoDao < longDaoDuSi) {
+            cout << "น้อยไปน้อง\n";
+        }
+        else {
+            cout << "มากไปพี่\n";
+        }
+    }
+
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
